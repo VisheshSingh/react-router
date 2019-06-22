fccfd71eaa47faaf20d790ba05571d0d4acda075
singhv <vishesh.singh@biworldwide.com>
2019-06-21 22:09:55 -0500

    Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
@@ -9,13 +10,13 @@ export default function Navbar() {
         </a>
         <ul className="right">
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
