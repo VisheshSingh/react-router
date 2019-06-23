beb4fc285b9e28362f102453748d1447d60f8acf
singhv <vishesh.singh@biworldwide.com>
2019-06-22 21:14:39 -0500

    making api calls

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Rainbow from '../hoc/Rainbow';
 
-function Home() {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint omnis
-        dignissimos doloremque soluta laboriosam tempore numquam quod voluptatem
-        earum? Expedita illum saepe est voluptates explicabo.
-      </p>
-    </div>
-  );
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    this.getPosts()
+      .then(data => {
+        console.log(data);
+        this.setState({
+          posts: [...data]
+        });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  getPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    return posts;
+  };
+
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div key={post.id} className="card post">
+            <div className="card-content">
+              <h4>{post.title}</h4>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <p>No posts yet...</p>
+    );
+    return (
+      <div className="container">
+        <h3 className="center">Home</h3>
+        {postList}
+      </div>
+    );
+  }
 }
 
 export default Rainbow(Home);
